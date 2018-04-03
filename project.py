2a109ad6c2090326214dd1a6e7e1b8809fa66cfc
huysteve <37971842+huysteve@users.noreply.github.com>
2018-04-03 23:14:15 +0700

    Update project.py

--- project.py
+++ project.py
@@ -22,12 +22,12 @@ if __name__ == "__main__":
                 .load()
         readTags.createOrReplaceTempView("tags")
       
-		#Thong ke so luong phim theo tung the loai
-		sqlDFb = spark.sql("Select genres, COUNT(movie_id) as total from movies GROUP BY genres")
-		sqlDFb.show()
-		#  Tim chat luong phim cua tung the loai
-		sqlDFb = spark.sql("Select AVG(rating) as avg_rating, genres from ratings, 
-		movies where movies.movie_id = ratings.movie_id group by genres order by AVG(rating) desc")
-		sqlDFb.show()
-		spark.stop()
+	#Thong ke so luong phim theo tung the loai
+	sqlDFb = spark.sql("Select genres, COUNT(movie_id) as total from movies GROUP BY genres")
+	sqlDFb.show()
+	#  Tim chat luong phim cua tung the loai
+	sqlDFb = spark.sql("Select AVG(rating) as avg_rating, genres from ratings, 
+	movies where movies.movie_id = ratings.movie_id group by genres order by AVG(rating) desc")
+	sqlDFb.show()
+	spark.stop()
 
