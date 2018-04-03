fff34efdb6cd0b03c22c1854b3263477dd0d1b0d
huysteve <37971842+huysteve@users.noreply.github.com>
2018-04-03 23:14:49 +0700

    Update project.py

--- project.py
+++ project.py
@@ -26,8 +26,7 @@ if __name__ == "__main__":
 	sqlDFb = spark.sql("Select genres, COUNT(movie_id) as total from movies GROUP BY genres")
 	sqlDFb.show()
 	#  Tim chat luong phim cua tung the loai
-	sqlDFb = spark.sql("Select AVG(rating) as avg_rating, genres from ratings, 
-	movies where movies.movie_id = ratings.movie_id group by genres order by AVG(rating) desc")
+	sqlDFb = spark.sql("Select AVG(rating) as avg_rating, genres from ratings, movies where movies.movie_id = ratings.movie_id group by genres order by AVG(rating) desc")
 	sqlDFb.show()
 	spark.stop()
 
